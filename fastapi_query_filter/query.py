b9d03baa946b29ac80652b351715401b0901abfe
Dmitry Vasiliev <contact.vasiliev.dmitry@gmail.com>
2023-01-28 20:08:10 +0300

    Separate validation logic into: _validate_amount, _validate_operator, _validate_value_type

--- fastapi_query_filter/query.py
+++ fastapi_query_filter/query.py
@@ -4,7 +4,6 @@ import typing
 from .utils.math import IntervalType
 from .types import (
     INCLUDE_OPERATORS,
-    QueryFilter,
     QueryFilterRequest,
     MORE_OPERATORS,
     LESS_OPERATORS,
@@ -24,11 +23,36 @@ class BaseQuery(abc.ABC):
 
     @classmethod
     @abc.abstractmethod
-    def validate(cls, queries: QueryFilterRequest, value_type: typing.Type):
+    def _validate_amount(cls, queries: QueryFilterRequest, value_type: typing.Type):
         """
-        Validate queries with common operator.
+        Validate amount queries.
         """
         pass
+
+    @classmethod
+    @abc.abstractmethod
+    def _validate_operator(cls, queries: QueryFilterRequest, value_type: typing.Type):
+        """
+        Validate queries operator.
+        """
+        pass
+
+    @classmethod
+    @abc.abstractmethod
+    def _validate_value_type(cls, queries: QueryFilterRequest, value_type: typing.Type):
+        """
+        Validate query value type.
+        """
+        pass
+
+    @classmethod
+    def validate(cls, queries: QueryFilterRequest, value_type: typing.Type):
+        """
+        Validate queries.
+        """
+        cls._validate_amount(queries, value_type)
+        cls._validate_operator(queries, value_type)
+        cls._validate_value_type(queries, value_type)
 
 
 class QueryType:
@@ -38,14 +62,19 @@ class QueryType:
             return queries[0].value
 
         @classmethod
-        def validate(cls, queries: typing.List[QueryFilter], value_type: typing.Type):
+        def _validate_amount(cls, queries: QueryFilterRequest, value_type: typing.Type):
             if len(queries) > 1:
                 raise ValueError("Query with such operator type must occur only once")
 
+        @classmethod
+        def _validate_operator(cls, queries: QueryFilterRequest, value_type: typing.Type):
             query = queries[0]
             if query.operator not in COMPARE_OPERATORS:
                 raise ValueError(f"Query '{query.field}' must use include operator")
 
+        @classmethod
+        def _validate_value_type(cls, queries: QueryFilterRequest, value_type: typing.Type):
+            query = queries[0]
             value = cls.interpret_value(queries)
             if not isinstance(value, value_type):
                 raise ValueError(f"Query '{query.field}' value has invalid type")
@@ -57,17 +86,19 @@ class QueryType:
             return IntervalType.from_list(values)
 
         @classmethod
-        def validate(cls, queries: typing.List[QueryFilter], value_type: typing.Type):
+        def _validate_amount(cls, queries: QueryFilterRequest, value_type: typing.Type):
             if len(queries) != 2:
                 raise ValueError("Query with such operator type must occur two times")
 
-            interval = cls.interpret_value(queries)
-
+        @classmethod
+        def _validate_operator(cls, queries: QueryFilterRequest, value_type: typing.Type):
             query_begin, query_end = sorted(queries, key=lambda query: query.value)
-
             if query_begin.operator not in MORE_OPERATORS or query_end.operator not in LESS_OPERATORS:
                 raise ValueError("Query must be interval.")
 
+        @classmethod
+        def _validate_value_type(cls, queries: QueryFilterRequest, value_type: typing.Type):
+            interval = cls.interpret_value(queries)
             if not (isinstance(interval.begin, value_type) and isinstance(interval.end, value_type)):
                 raise ValueError("Query value has invalid type")
 
@@ -77,14 +108,19 @@ class QueryType:
             return queries[0].value
 
         @classmethod
-        def validate(cls, queries: typing.List[QueryFilter], value_type: typing.Type):
+        def _validate_amount(cls, queries: QueryFilterRequest, value_type: typing.Type):
             if len(queries) > 1:
                 raise ValueError("Query with such operator type must occur only once")
 
+        @classmethod
+        def _validate_operator(cls, queries: QueryFilterRequest, value_type: typing.Type):
             query = queries[0]
             if query.operator not in INCLUDE_OPERATORS:
                 raise ValueError(f"Query '{query.field}' must use include operator")
 
+        @classmethod
+        def _validate_value_type(cls, queries: QueryFilterRequest, value_type: typing.Type):
+            query = queries[0]
             value = cls.interpret_value(queries)
             is_valid_type = (isinstance(val, value_type) for val in value)
             if not isinstance(value, list) or not all(is_valid_type):
@@ -96,14 +132,19 @@ class QueryType:
             return queries[0].value
 
         @classmethod
-        def validate(cls, queries: typing.List[QueryFilter], value_type: typing.Type):
+        def _validate_amount(cls, queries: QueryFilterRequest, value_type: typing.Type):
             if len(queries) > 1:
                 raise ValueError("Query with such operator type must occur only once")
 
+        @classmethod
+        def _validate_operator(cls, queries: QueryFilterRequest, value_type: typing.Type):
             query = queries[0]
             if query.operator is QueryFilterOperators.OPTION:
                 raise ValueError(f"Query '{query.field}' use only '{QueryFilterOperators.OPTION}' operator")
 
+        @classmethod
+        def _validate_value_type(cls, queries: QueryFilterRequest, value_type: typing.Type):
+            query = queries[0]
             value = cls.interpret_value(queries)
             if not isinstance(value, bool):
                 raise ValueError(f"Query '{query.field}' value has invalid type")
